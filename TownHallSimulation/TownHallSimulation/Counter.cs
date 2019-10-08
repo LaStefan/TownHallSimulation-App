20a8ff7dac1c2c18b9c02b0b5f50e5df0e303bb8
Stefan Lazarevic <s.lazarevic@student.fontys.nl>
2019-10-08 11:51:39 +0200

    Counter class added

---
+++ TownHallSimulation/TownHallSimulation/Counter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TownHallSimulation
+{
+    class Counter
+    {
+        private int id;
+        public Point location;
+        public bool isOpened;
+        public bool isOccupied;
+
+        public Counter(Point loc)
+        {
+            this.location = loc;
+        }
+
+
+
+        public void OpenCounter()
+        {
+            isOpened = true;
+        }
+        public void CloseCounter()
+        {
+            isOpened = false;
+        }
+        public void UpdateStatus()
+        {
+            if(isOccupied == false)
+            {
+                isOccupied = true;
+            }
+            else
+            {
+                isOccupied = false;
+            }
+        }
+
+    }
+}

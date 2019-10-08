03423f5a47ebbbe7d6cd33bc4a7b34883cb890ad
Jane <ezehchioma908@gmail.com>
2019-10-08 14:47:05 +0200

    Update person class

--- TownHallSimulation/TownHallSimulation/Person.cs
+++ TownHallSimulation/TownHallSimulation/Person.cs
@@ -3,27 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace TownHallSimulation
 {
-    public class Person
-    {
-        private static int counter = 0;
-
-        public Person(Appointment appointment)
-        {
-            counter++;
-            this.GetAppointment = appointment;
-        }
-        public Appointment GetAppointment { get; }
-        public int GetId { get; } = counter;
-    }
-
-    //Types of appointments... to be discussed.
+    //Types of appointments... 
     public enum Appointment
     {
         AddressChange,
         PropertySale,
         PermitRequest
+    }
+    public class Person
+    {
+        private static int counter = 0;
+        private int id = 0;
+        public Point Location { get; private set; }
+        public Point[] PathToFollow { get; private set; }
+        public Bitmap Image { get; private set; }
+
+        public Person(Appointment appointment, Point location, Bitmap image)
+        {
+            counter++;
+            this.GetAppointment = appointment;
+            this.Location = location;
+            this.Image = image;
+        }
+        public Appointment GetAppointment { get; }
+        public int GetId { get; } = counter;
+
+        public int GetPersonId()
+        {
+            return this.id;
+        }
+
+        public void DrawPerson(Graphics gr)
+        {
+            gr.DrawImage(Image, Location);
+        }
+
+       // public bool GoToCounter()
+       //{
+            
+       // }
+
+
+
+
     }
 }
